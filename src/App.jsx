8c12ec4c86cbd74061245d4fa0aeb2beb2998d5e
gabriel <galhiardogabriel@gmail.com>
2025-06-04 10:41:42 -0300

    feat: adicionando deletarTarefas e listar como concluida

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,53 @@ import Tarefas from './components/tarefas.jsx';
 import AdicionarTarefa from './components/AdicionarTarefa.jsx'; 
 
 function App(){
+  const [tarefas, setTarefas] = useState([{
+    id: 1,
+    titulo: "Estudar React",
+    descricao: "Estudar os conceitos básicos do React, incluindo componentes, props e state.",
+    concluida: false
+  }, {
+    id: 2,
+    titulo: "Praticar JavaScript",
+    descricao: "Resolver exercícios de JavaScript para melhorar a lógica de programação.",
+    concluida: false
+  }, {
+    id: 3,
+    titulo: "Ler Documentação",
+    descricao: "Ler a documentação oficial do React para entender melhor as APIs.",
+    concluida: false
+  }
+]);
 
-  //state (estado) é uma variável que pode ser alterada
-  //e quando alterada, o componente é re-renderizado
-  //useState é um Hook que permite adicionar estado a componentes funcionais
+  function quandoClicaNaTarefa(tarefaId) {
+    const novaTarefa = tarefas.map((tarefa) => {
+      if(tarefaId === tarefa.id) {
+        return {
+          ...tarefa,
+          concluida: !tarefa.concluida
+        }
+      }
+      return tarefa;
+    })
+    setTarefas(novaTarefa);
+  }
+
+  function deletarTarefa(tarefaId) {
+    const novasTarefas = tarefas.filter((tarefa) => tarefa.id !== tarefaId);
+    setTarefas(novasTarefas);
+  }
+  
 
 
   return(
-   <div>
-    <h1 className="text-red-500 text-4xl">Gerenciador de Tarefas</h1>
-    <AdicionarTarefa />
-    <Tarefas />
+   <div className="w-screen h-screen flex flex-col items-center justify-center bg-slate-500 p-6">
+    <div className="w-[500px]">
+      <h1 className="text-3xl text-slate-100 font-bold text-center">
+        Gerenciador de Tarefas
+      </h1>
+    <AdicionarTarefa/>
+    <Tarefas tarefas={tarefas} quandoClicaNaTarefa={quandoClicaNaTarefa} deletarTarefa={deletarTarefa}/>
+    </div>
 
    </div>
   )
--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -1,8 +1,27 @@
-function Tarefas(){
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tarefas({tarefas, quandoClicaNaTarefa, deletarTarefa}) {
     return (
-        <div>
-            <h1>Tarefas</h1>
-        </div>
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">{tarefas.map((tarefas) => (
+            <li key={tarefas.id} className="flex gap-2" >
+                <button 
+                onClick={() => quandoClicaNaTarefa(tarefas.id)} 
+                className={`bg-slate-400 text-white p-2 rounded-md w-full text-left ${tarefas.concluida && "line-through"}`}>
+                    {tarefas.titulo}
+                    </button>
+                
+                <button className="bg-slate-400 text-white p-2 rounded-md ">
+                    <ChevronRightIcon/>
+                </button>
+
+                 <button onClick={() => deletarTarefa(tarefas.id)} className="bg-slate-400 text-white p-2 rounded-md">
+                    <TrashIcon/>
+                </button>
+
+
+            </li>
+        )
+        )}</ul>
     );
 }
 
